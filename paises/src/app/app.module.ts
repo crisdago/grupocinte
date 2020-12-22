b24bccc6ce89fceeecfa712f7ebbd168222eee47
crisdago <crisdago@hotmail.com>
2020-12-21 20:10:33 -0500

    Creacion componente y servicio

--- paises/src/app/app.module.ts
+++ paises/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaPaisesComponent } from './public/lista-paises/lista-paises.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaPaisesComponent
   ],
   imports: [
     BrowserModule,
---
+++ paises/src/app/public/lista-paises/lista-paises.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-paises',
+  templateUrl: './lista-paises.component.html',
+  styleUrls: ['./lista-paises.component.css']
+})
+export class ListaPaisesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
